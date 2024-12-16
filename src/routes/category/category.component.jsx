9fcb837e267bed44777541d7fd41ac3862c69b47
Sayyid Marvan <sayyidmarvanvt@gmail.com>
2024-12-16 17:04:46 +0300

    added variables in graphql

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -1,21 +1,47 @@
-import { useContext, useState, useEffect, Fragment } from "react";
+import { useState, useEffect, Fragment } from "react";
 import { useParams } from "react-router-dom";
+import { gql, useQuery } from "@apollo/client";
 
+import Spinner from "../../components/spinner/spinner.component";
 import ProductCard from "../../components/product-card/product-card.component";
 
-import { CategoriesContext } from "../../contexts/categories.context";
-
 import { CategoryContainer, Title } from "./category.styles";
-import Spinner from "../../components/spinner/spinner.component";
+
+const GET_CATEGORY = gql`
+  query ($title: String!) {
+    getCollectionsByTitle(title: $title) {
+      id
+      title
+      items {
+        id
+        name
+        price
+        imageUrl
+      }
+    }
+  }
+`;
 
 const Category = () => {
   const { category } = useParams();
-  const { categoriesMap, loading } = useContext(CategoriesContext);
-  const [products, setProducts] = useState(categoriesMap[category]);
+  const { loading, error, data } = useQuery(GET_CATEGORY, {
+    variables: {
+      title: category,
+    },
+  });
+
+  const [products, setProducts] = useState([]);
 
   useEffect(() => {
-    setProducts(categoriesMap[category]);
-  }, [category, categoriesMap]);
+    if (data) {
+      const {
+        getCollectionsByTitle: { items },
+      } = data;
+      setProducts(items);
+    } else {
+      console.log(error);
+    }
+  }, [category, data]);
 
   return (
     <Fragment>
